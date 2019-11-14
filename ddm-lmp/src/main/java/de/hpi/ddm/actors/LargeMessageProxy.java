23235519ee94f2396fb40a4635dc79edf83d77ec
Richard Ebeling <He3lixxx@users.noreply.github.com>
2019-11-14 18:00:33 +0100

    Minor comment improvements / code styling

--- ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
+++ ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
@@ -82,19 +82,19 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	/////////////////
 
 	/*
-		Note: UUIDs are Strings to insure serialization
+	Note: UUIDs are Strings to ensure serialization
 
-		For each UUID, we need an own byte stream that we write into when we receive chunks of
-	 	large messages.
-	 	This is necessary so that multiple senders can simultaneously send multiple different
-	 	messages through the same proxy and maybe even to the same receiver.
+	For each UUID, we need an own byte stream that we write into when we receive chunks of
+	large messages.
+	This is necessary so that multiple senders can simultaneously send multiple different
+	messages through the same proxy and maybe even to the same receiver.
 	*/
 	private Map<String, ByteArrayOutputStream> incomingByteStreams = new HashMap<>();
 
 	/*
 	Map from UUID to MessageContext for the sender.
-	Each SenderProxy needs to remember what bytes are left to send for each large message.
-	Also the SenderProxy should save the sender/receiver pair once so that the byte messages
+	Each sending LargeMessageProxy needs to remember what bytes are left to send for each large message.
+	Also the sending LargeMessageProxy should save the sender/receiver pair so that the byte messages
 	can be sent without them.
 	*/
 	private Map<String, MessageContext> sendersMemory = new HashMap<>();
@@ -123,101 +123,101 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 
 	/*
 	This is the LargeMessage that the Sender wants to send via the LargeMessageProxy. It is sent over the
-	large message side channel
+	large message side channel.
 
-	This message type is processed by the SenderProxy.
+	This message type is processed by the sending LargeMessageProxy.
 	 */
 	private void handle(LargeMessage<?> message) throws IOException{
 		ActorRef receiver = message.getReceiver();
-		ActorSelection receiverProxy = this.context().actorSelection(receiver.path().child(DEFAULT_NAME));
+		ActorSelection receiving_proxy = this.context().actorSelection(receiver.path().child(DEFAULT_NAME));
 
-		//create the streams for serialization from Object -> Bytes
+		// create the streams for serialization from Object -> Bytes
 		ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
 		ObjectOutputStream objectStream = new ObjectOutputStream(byteStream);
 
-		//write the message in the ObjectStream which writes it in the ByteStream
+		// write the message in the ObjectStream which writes it in the ByteStream
 		objectStream.writeObject(message.getMessage());
 
-		//Extract the serialized message from the StreamObject as a byte array
+		// extract the serialized message from the StreamObject as a byte array
 		byte[] bytes = byteStream.toByteArray();
 
-		//Each LargeMessage has its own UUID
+		// each LargeMessage has its own UUID
 		String uuid = UUID.randomUUID().toString();
 
-		//create a stream where the message (in bytes) can be read from
+		// create a stream where the message (in bytes) can be read from
 		//-> easier way of remembering which bytes have been sent already
 		ByteArrayInputStream serializedMessage = new ByteArrayInputStream(bytes);
 
-		//The SenderProxy has to remember the context around the LargeMessage for later
+		// the sending LargeMessageProxy has to remember the context around the LargeMessage for later
 		sendersMemory.put(uuid, new MessageContext(this.sender(), message.getReceiver(), serializedMessage));
 
-		//Initiate the pulling of the ReceiverProxy with a HaveDataMessage
-		receiverProxy.tell(new HaveDataMessage(uuid), this.self());
+		// initiate the pulling of the receiving LargeMessageProxy with a HaveDataMessage
+		receiving_proxy.tell(new HaveDataMessage(uuid), this.self());
 	}
 
 	/*
-	The HaveDataMessage is more of a signal that indicates that there is still data left to pull
-	from the SenderProxy. That's why the handling of the HaveDataMessage is an immediate response with
-	a WantDataMessage from the ReceiverProxy to the SenderProxy.
+	The HaveDataMessage is a signal that indicates that there is still data left to pull
+	from the sending LargeMessageProxy. That's why the handling of the HaveDataMessage is an immediate response with
+	a WantDataMessage from the receiving LargeMessageProxy to the sending LargeMessageProxy.
 
 	Note: If there are many large messages to transfer this "pull" paradigm might also lead to an inbox overflow
-	since many large messages would automatically send the HaveDataMessages which automatically trigger the
+	since many large messages would send the HaveDataMessages which instantaneously trigger the
 	WantDataMessages. So one could say it is a lazy pull paradigm. For overflow protection there needs to be a
 	mailbox check of some sort before WantDataMessages are sent out.
 
-	This message type is processed by the ReceiverProxy.
+	This message type is processed by the receiving LargeMessageProxy.
 	 */
 	private void handle(HaveDataMessage message){
 		this.sender().tell(new WantDataMessage(message.getUuid()), this.self());
 	}
 
 	/*
-	The WantDataMessage is a signal that indicates that the ReceiverProxy processed all ByteMessages
+	The WantDataMessage is a signal that indicates that the receiving LargeMessageProxy processed all ByteMessages
 	that came in a single batch for the one particular LargeMessage referenced by the UUID. It results in
-	the SenderProxy sending another batch of ByteMessages together with a
+	the sending LargeMessageProxy sending another batch of ByteMessages together with a
 		- HaveDataMessage if there is still data left
 		- DoneMessage if there is no data left
 	of the LargeMessage.
 
-	This message type is processed by the SenderProxy.
+	This message type is processed by the sending LargeMessageProxy.
 	 */
 	private void handle(WantDataMessage message){
-		//get the context corresponding to the message from the ReceiverProxy
+		// get the context corresponding to the message from the receiving LargeMessageProxy
 		MessageContext context = sendersMemory.get(message.getUuid());
 		if (context == null){
 			this.log().info("Received unexpected WantDataMessage: \"{}\"", message.toString());
 			return;
 		}
 
-		//send another batch to the ReceiverProxy
+		// send another batch to the receiving LargeMessageProxy
 		for(int i = 0; i < BATCH_SIZE; ++i) {
-			//important since the message is not always dividable by the CHUNK_SIZE
+			// important since the message is not always divisible by the CHUNK_SIZE
 			int arrayLength = Math.min(context.getData().available(), CHUNK_SIZE);
 
-			//there is still data left to send
+			// there is still data left to send
 			if (arrayLength != 0) {
-				//the context includes the stream where data is read from -> read one chunk from it and save it on a variable
+				// the context includes the stream where data is read from -> read one chunk from it
 				byte[] chunk = new byte[arrayLength];
 				int bytesRead = context.getData().read(chunk, 0, arrayLength);
 				assert(bytesRead == arrayLength);
 
-				//send the chunk as a ByteMessage to the ReceiverProxy
+				// send the chunk as a ByteMessage to the receiving LargeMessageProxy
 				this.sender().tell(new BytesMessage<>(chunk, message.getUuid()), this.self());
 			} else {
-				//since there is no data left the SenderProxy sends a DoneMessage with all the
-				//context information from the corresponding LargeMessage so that the ReceiverProxy
-				//can de-serialize the message and forward it to the receiver
+				// since there is no data left the sending LargeMessageProxy sends a DoneMessage with all the
+				// context information from the corresponding LargeMessage so that the receiving LargeMessageProxy
+				// can de-serialize the message and forward it to the receiver
 				this.sender().tell(new DoneMessage(
 						context.getSender(), context.getReceiver(), message.getUuid()
 				), this.self());
-				//remove the entry since this communication is done
+				// remove the entry since this communication is done
 				sendersMemory.remove(message.getUuid());
-				//returning from this function prevents sending another HaveDataMessage
+				// returning from this function prevents sending another HaveDataMessage
 				return;
 			}
 		}
-		//after sending one batch, immediately send a signal to the ReceiverProxy that more data is available
-		//for overflow prevention it is important that these messages are sent in order -> TCP
+		// after sending one batch, immediately send a signal to the receiving LargeMessageProxy that more data is available
+		// for overflow prevention it is important that these messages are sent in order -> TCP
 		this.sender().tell(new HaveDataMessage(message.getUuid()), this.self());
 	}
 
@@ -225,19 +225,19 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	The ByteMessages handle the actual payload that is to be sent. Unnecessary information like sender and
 	receiver is dropped since this is all handled in other messages.
 
-	This message type is processed by the ReceiverProxy.
+	This message type is processed by the receiving LargeMessageProxy.
 	 */
 	private void handle(BytesMessage<?> message) {
-		//find the stream that is responsible for this large message (UUID)
+		// find the stream that is responsible for this large message
 		ByteArrayOutputStream stream = incomingByteStreams.get(message.getUuid());
 
-		//this is the first message of the large message -> create a new stream and a new mapping entry
+		// this is the first message of the large message -> create a new stream and a new mapping entry
 		if (stream == null) {
 			stream = new ByteArrayOutputStream();
 			incomingByteStreams.put(message.getUuid(), stream);
 		}
 
-		//write the bytes into the stream
+		// write the bytes into the stream
 		byte[] messageBytes = (byte[]) message.getBytes();
 		stream.write(messageBytes, 0, messageBytes.length);
 	}
@@ -246,25 +246,23 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	The DoneMessage signals the end of a large message transfer. This means that the object can be
 	de-serialized and forwarded to the receiver.
 
-	This message type is processed by the ReceiverProxy.
+	This message type is processed by the receiving LargeMessageProxy.
 	 */
 	private void handle(DoneMessage message) throws IOException {
-		//look up the stream that has the complete information
+		// look up the stream that should have received the serialized object by now
 		ByteArrayOutputStream stream;
 		stream = incomingByteStreams.get(message.getUuid());
-		if (stream==null){
+		if (stream == null){
 			this.log().info("Received unexpected DoneMessage: \"{}\"", message.toString());
 			return;
 		}
 
-		//read the bytes from the stream and de-serialize it to an Object
+		// read the bytes from the stream and deserialize the object
 		byte[] bytes = stream.toByteArray();
 		ByteArrayInputStream byteStream = new ByteArrayInputStream(bytes);
 		ObjectInputStream objectStream = new ObjectInputStream(byteStream);
 		try {
-			//de-serialization
 			Object object = objectStream.readObject();
-			//forwarding
 			message.getReceiver().tell(object, message.getSender());
 		} catch (ClassNotFoundException e) {
 			this.log().info("Error while trying to deserialize sent bytes when handling DoneMessage");
